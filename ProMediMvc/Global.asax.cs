a6f2f1d610c99ef7a545425264f5a3c28eef97db
Nurana Rustamova <nuranaar@code.edu.az>
2019-05-23 13:20:13 +0400

    bundles

--- ProMediMvc/Global.asax.cs
+++ ProMediMvc/Global.asax.cs
@@ -15,7 +15,7 @@ namespace ProMediMvc
 			AreaRegistration.RegisterAllAreas();
 			FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
 			RouteConfig.RegisterRoutes(RouteTable.Routes);
-			BundleConfig.RegisterBundles(BundleTable.Bundles);
+		//	BundleConfig.RegisterBundles(BundleTable.Bundles);
 		}
 	}
 }
